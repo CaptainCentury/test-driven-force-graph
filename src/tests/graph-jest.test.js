c0be9fdc3985e5aa066225514bd0aef485f9c07a
Paul Sjöberg <paul.g.sjoberg@gmail.com>
2022-06-04 16:00:58 +0200

    Separate pure jest tests from testing library

---
+++ src/tests/graph-jest.test.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+import { ForceGraph } from "../components/force-graph";
+
+describe("JSX", () => {
+  it("calls React.createElement", () => {
+    const createElementSpy = jest.spyOn(React, "createElement");
+    <h1>Hello, JSX!</h1>;
+    expect(createElementSpy).toHaveBeenCalledWith("h1", null, "Hello, JSX!");
+  });
+});
+
+describe("ForceGraph", () => {
+  it("creates ForceGraph component", () => {
+    const createElementSpy = jest.spyOn(React, "createElement");
+    <ForceGraph>example</ForceGraph>;
+    expect(createElementSpy).toHaveBeenCalledWith(ForceGraph, null, "example");
+  });
+});
--- src/tests/graph-testing-library.test.js
+++ src/tests/graph-testing-library.test.js
@@ -7,22 +7,6 @@ import { ForceGraph } from "../components/force-graph";
 
 import { render, screen } from "@testing-library/react";
 
-describe("JSX", () => {
-  it("calls React.createElement", () => {
-    const createElementSpy = jest.spyOn(React, "createElement");
-    <h1>Hello, JSX!</h1>;
-    expect(createElementSpy).toHaveBeenCalledWith("h1", null, "Hello, JSX!");
-  });
-});
-
-describe("ForceGraph", () => {
-  it("creates ForceGraph component", () => {
-    const createElementSpy = jest.spyOn(React, "createElement");
-    <ForceGraph>example</ForceGraph>;
-    expect(createElementSpy).toHaveBeenCalledWith(ForceGraph, null, "example");
-  });
-});
-
 describe("ForceGraph in React Testing Library", () => {
   it("renders", () => {
     render(<ForceGraph />);
