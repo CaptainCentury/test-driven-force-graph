fcb36252c35b977fa09364b8215a66ba6e915c99
Paul Sjöberg <paul.g.sjoberg@gmail.com>
2022-06-06 21:36:24 +0200

    Refactoring, breaking out duplicatyes in tests

--- src/tests/graph-testing-library.test.js
+++ src/tests/graph-testing-library.test.js
@@ -7,14 +7,19 @@ import ForceGraph from "../components/force-graph";
 
 import { render, screen } from "@testing-library/react";
 
-describe("ForceGraph in React Testing Library", () => {
-  it("renders", () => {
+describe("ForceGraph with empty dataset", () => {
+  beforeEach(() => {
     const emptyData = { nodes: [], edges: [] };
     render(
       <div data-testid="element">
-        <ForceGraph dataset={emptyData} />
+        <ForceGraph data-testid="element" dataset={emptyData}>
+          CHILDREN
+        </ForceGraph>
       </div>
     );
+  });
+
+  it("renders", () => {
     const element = screen.getByTestId("element");
     expect(element.tagName).toBe("DIV");
     expect(element.children[0].tagName).toBe("svg");
@@ -22,35 +27,30 @@ describe("ForceGraph in React Testing Library", () => {
   });
 
   it("passes children through", () => {
-    const emptyData = { nodes: [], edges: [] };
-    render(<ForceGraph dataset={emptyData}>CHILDREN</ForceGraph>);
     expect(screen.getByText(/CHILDREN/i).tagName).toBe("DIV");
+  });
+});
+
+describe("ForceGraph with simple dataset", () => {
+  let rerender;
+  const data = {
+    nodes: [{ name: "A" }, { name: "B" }, { name: "C" }],
+    edges: [
+      { source: 0, target: 1 },
+      { source: 0, target: 2 },
+      { source: 1, target: 2 },
+    ],
+  };
+  beforeEach(() => {
+    ({ rerender } = render(<ForceGraph dataset={data} />));
   });
 
   it("renders data", async () => {
-    const data = {
-      nodes: [{ name: "A" }, { name: "B" }, { name: "C" }],
-      edges: [
-        { source: 0, target: 1 },
-        { source: 0, target: 2 },
-        { source: 1, target: 2 },
-      ],
-    };
-    render(<ForceGraph dataset={data} />);
     const element = await screen.findByText("A");
     expect(element.parentElement.tagName).toBe("circle");
   });
 
   it("updates graph with new data", () => {
-    const data = {
-      nodes: [{ name: "A" }, { name: "B" }, { name: "C" }],
-      edges: [
-        { source: 0, target: 1 },
-        { source: 0, target: 2 },
-        { source: 1, target: 2 },
-      ],
-    };
-    const { rerender } = render(<ForceGraph dataset={data} />);
     const updatedData = { ...data };
     updatedData.nodes.push({ name: "D" });
     updatedData.edges.concat([
