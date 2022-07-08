7785a75035ca68da083837c656c401962f5af525
Paul Sjöberg <paul.g.sjoberg@gmail.com>
2022-07-08 19:39:30 +0200

    Adds stub for toggle list demo

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,14 @@ import { createRoot } from "react-dom/client";
 import { GraphDemo } from "./graph-demo";
 import { LinePlotDemo } from "./plot-demo";
 import React from "react";
+import { ToggleListDemo } from "./toggle-list-demo";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 
 root.render(
   <>
+    <ToggleListDemo />
     <GraphDemo />
     <LinePlotDemo />
   </>
---
+++ src/toggle-list-demo.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { ToggleList } from "./components/toggle-list";
+
+export const ToggleListDemo = () => {
+  return (
+    <>
+      <h1>Toggle list example</h1>
+      <ToggleList />
+    </>
+  );
+};
