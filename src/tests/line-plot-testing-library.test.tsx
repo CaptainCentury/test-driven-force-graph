f5a15abdeebeb47710be4ebe5a270f074d962c0d
Paul Sjöberg <paul.g.sjoberg@gmail.com>
2022-07-08 14:14:17 +0200

    Increase test voverage for line plot

--- src/tests/line-plot-testing-library.test.tsx
+++ src/tests/line-plot-testing-library.test.tsx
@@ -4,16 +4,32 @@
 import React from "react";
 import { LinePlot } from "../components/line-plot";
 import { render } from "@testing-library/react";
-import { twoLinesDataPromise } from "../data/two-lines-data";
 import { tsvParse } from "d3";
 
 describe("LinePlot", () => {
-  let data = tsvParse("x1\ty1\ty2\n1.0\t0.5\t0.25\n2.0\t0.5\t0.5");
+  const empty = tsvParse("");
+  const data = tsvParse("x1\ty1\ty2\n1.0\t0.5\t0.25\n2.0\t0.5\t0.5");
 
   it("renders", () => {
     const testId = "plot1";
     const { getByTestId } = render(
       <LinePlot data-testid={testId} dataTable={data} />
+    );
+    expect(getByTestId(testId)).toMatchSnapshot();
+  });
+
+  it("renders for empty data", () => {
+    const testId = "plot1";
+    const { getByTestId } = render(
+      <LinePlot data-testid={testId} dataTable={empty} />
+    );
+    expect(getByTestId(testId)).toMatchSnapshot();
+  });
+
+  it("renders for undefined data", () => {
+    const testId = "plot1";
+    const { getByTestId } = render(
+      <LinePlot data-testid={testId} dataTable={undefined} />
     );
     expect(getByTestId(testId)).toMatchSnapshot();
   });
