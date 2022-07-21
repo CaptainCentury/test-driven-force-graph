8e6ec9da94c01d709f49963b024aab1cae00a12a
Paul Sjöberg <paul.g.sjoberg@gmail.com>
2022-07-21 16:16:39 +0200

    Adds svg element for doughnut diagram

--- src/components/doughnut-diagram.tsx
+++ src/components/doughnut-diagram.tsx
@@ -1,8 +1,21 @@
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, useRef } from "react";
 
 export const DoughnutDiagram: FunctionComponent = () => {
+  const svgRef = useRef(null);
+  const margin = { top: 30, right: 30, bottom: 30, left: 30 };
+
+  // define display
+  var w = 300;
+  var h = 300;
+
   return (
     <figure>
+      <svg
+        ref={svgRef}
+        style={{ borderStyle: "solid", borderWidth: "1px" }}
+        width={w + margin.left + margin.right}
+        height={h + margin.top + margin.bottom}
+      />
       <figcaption>DONUGHT DIAGRAM</figcaption>
     </figure>
   );
