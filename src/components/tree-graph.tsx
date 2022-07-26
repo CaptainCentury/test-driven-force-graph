0bcb4d70f9febb0e425361005afd0e2135236328
Paul Sjöberg <paul.g.sjoberg@gmail.com>
2022-07-26 18:53:00 +0200

    BAsic implementation of tree graph

--- src/components/tree-graph.tsx
+++ src/components/tree-graph.tsx
@@ -1,4 +1,5 @@
-import React, { FunctionComponent, useRef } from "react";
+import React, { FunctionComponent, useEffect, useRef, useState } from "react";
+import { hierarchy, linkVertical, select, tree } from "d3";
 
 type TreeGraphProps = {
   children?: string | JSX.Element | JSX.Element[];
@@ -8,10 +9,43 @@ export const TreeGraph: FunctionComponent<TreeGraphProps> = ({
   children,
   ...props
 }) => {
+  const [noNodes, setNoNodes] = useState(0);
   const svgRef = useRef(null);
   const margin = { top: 30, right: 30, bottom: 30, left: 30 };
   const w = 300;
   const h = 300;
+
+  useEffect(() => {
+    const data = { children: [{ children: [] }, { children: [] }] };
+    const nodes = hierarchy(data, (d) => d.children);
+    tree().size([250, 250])(nodes);
+
+    nodes.count();
+    setNoNodes(nodes.value ?? 0);
+    const g = select(svgRef.current)
+      .append("g")
+      .attr("transform", "translate(25, 25)");
+
+    const lineMaker = linkVertical<any, { x: number; y: number }>()
+      .x((d) => d.x)
+      .y((d) => d.y);
+
+    g.selectAll("path")
+      .data(nodes.links())
+      .enter()
+      .append("path")
+      .attr("d", (d) => lineMaker(d))
+      .attr("stroke", "red")
+      .attr("fill", "none");
+
+    g.selectAll("circle")
+      .data<any>(nodes.descendants())
+      .enter()
+      .append("circle")
+      .attr("r", 5)
+      .attr("cx", (d) => d.x)
+      .attr("cy", (d) => d.y);
+  }, []);
 
   return (
     <figure {...props}>
@@ -26,7 +60,9 @@ export const TreeGraph: FunctionComponent<TreeGraphProps> = ({
         width={w + margin.left + margin.right}
         height={h + margin.top + margin.bottom}
       />
-      <figcaption>TREE GRAPH () {children}</figcaption>
+      <figcaption>
+        TREE GRAPH ({noNodes}) {children}
+      </figcaption>
     </figure>
   );
 };
