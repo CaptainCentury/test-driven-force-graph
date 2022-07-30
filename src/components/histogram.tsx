4bc6daf290037b44655a853e94c1a459d041acac
Paul Sjöberg <paul.g.sjoberg@gmail.com>
2022-07-30 18:18:45 +0200

    Basic implementation of histogram component

--- src/components/histogram.tsx
+++ src/components/histogram.tsx
@@ -1,4 +1,15 @@
-import React, { FunctionComponent, useEffect, useRef } from "react";
+import React, { FunctionComponent, useEffect, useRef, useState } from "react";
+import {
+  axisLeft,
+  bin,
+  extent,
+  max,
+  min,
+  scaleBand,
+  scaleLinear,
+  select,
+  thresholdScott,
+} from "d3";
 
 type HistogramProps = {
   data: number[];
@@ -10,12 +21,60 @@ export const Histogram: FunctionComponent<HistogramProps> = ({
   children,
   ...props
 }) => {
+  const [noBins, setNoBins] = useState(0);
   const svgRef = useRef(null);
   const margin = { top: 30, right: 30, bottom: 30, left: 30 };
   const w = 300;
   const h = 300;
 
-  useEffect(() => {}, [data]);
+  useEffect(() => {
+    const histogram = bin()
+      .thresholds(thresholdScott(data, min(data), max(data)))
+      .value((d: any) => d)(data);
+    setNoBins(histogram.length);
+
+    const xScale = scaleBand()
+      .padding(0.2)
+      .round(true)
+      .range([0, w])
+      .domain(histogram.map((d) => ((d.x0 + d.x1) / 2).toString()));
+
+    const yScale = scaleLinear()
+      .range([h, 0])
+      .domain([0, max(histogram, (d) => d.length)])
+      .nice();
+
+    const g = select(svgRef.current)
+      .append("g")
+      .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+    g.selectAll("rect")
+      .data(histogram)
+      .enter()
+      .append("rect")
+      .attr("width", xScale.bandwidth())
+      .attr("x", (d: any) => xScale(((d.x0 + d.x1) / 2).toString()))
+      .attr("y", (d) => yScale(d.length))
+      .attr("height", (d) => h - yScale(d.length))
+      .attr("fill", "red")
+      .attr("fill-opacity", 0.2)
+      .attr("stroke", "red")
+      .attr("stroke-width", 2);
+
+    const fontSize = 14;
+    g.selectAll("text")
+      .data(histogram)
+      .enter()
+      .append("text")
+      .attr("text_anchor", "middle")
+      .attr("font-family", "sans-serif")
+      .attr("font-size", fontSize)
+      .attr("x", (d) => xScale(((d.x0 + d.x1) / 2).toString()))
+      .attr("y", h + 1.25 * fontSize)
+      .text((d) => `[${d.x0}, ${d.x1}]`);
+
+    g.append("g").call(axisLeft(yScale));
+  }, [data]);
 
   return (
     <figure {...props}>
@@ -31,7 +90,7 @@ export const Histogram: FunctionComponent<HistogramProps> = ({
         height={h + margin.top + margin.bottom}
       />
       <figcaption>
-        HISTOGRAM ({data.length} samples) {children}
+        HISTOGRAM ({data.length} samples in {noBins} bins) {children}
       </figcaption>
     </figure>
   );
--- src/histogram-demo.tsx
+++ src/histogram-demo.tsx
@@ -5,7 +5,9 @@ export const HistogramDemo = () => {
   return (
     <>
       <h1>Histogram demo</h1>
-      <Histogram data={[]} />
+      <Histogram
+        data={[1, 1.1, 1.2, 2, 2, 2.1, 2.2, 2.2, 2.3, 2.3, 3, 3, 3, 4, 4, 4, 4]}
+      />
     </>
   );
 };
