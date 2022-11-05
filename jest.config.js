ce291f38170e2a138243d99caf1a4c41f8778ad4
Paul Sjöberg <paul.g.sjoberg@gmail.com>
2022-11-05 18:22:45 +0100

    Remove test for loading data - didn't get it to work

--- jest.config.js
+++ jest.config.js
@@ -10,9 +10,6 @@ module.exports = {
   transformIgnorePatterns: ["node_modules/(?!@ngrx|(?!deck.gl)|ng-dynamic)"],
   setupFiles: ["./src/tests/setup-tests.js"],
   setupFilesAfterEnv: ["./src/tests/setup-framework.js"],
-  moduleNameMapper: {
-    "two-lines-data": "../tests/__mocks__/two-lines-data.ts",
-  },
   //   moduleNameMapper: {
   //     d3: "./node_modules/d3/dist/d3.min.js",
   //   },
--- src/tests/__mocks__/two-lines-data.ts
+++
@@ -1,3 +0,0 @@
-import { tsv } from "d3";
-
-export const twoLinesDataPromise = tsv("../../public/two-lines-table.tsv");
