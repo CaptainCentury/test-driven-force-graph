b239cf8c6d7bd95ad757e29fc1086c95bd36cbe3
Paul Sjöberg <paul.g.sjoberg@gmail.com>
2022-11-06 22:43:22 +0100

    Tidying up force graph story

--- src/components/ForceGraph.stories.tsx
+++ src/components/ForceGraph.stories.tsx
@@ -6,22 +6,18 @@ import namesData from "../data/names";
 import abcData from "../data/abc";
 import { yarnball } from "../data/network-helper";
 
-// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
   title: "Example/ForceGraph",
   component: ForceGraph,
-  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
   argTypes: {
-    backgroundColor: { control: "color" },
+    labelMode: { control: "select", options: ["labels", "tooltip"] },
   },
 } as ComponentMeta<typeof ForceGraph>;
 
-// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 const Template: ComponentStory<typeof ForceGraph> = (args) => (
   <ForceGraph {...args} />
 );
 
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
 export const abc = Template.bind({});
 abc.args = {
   style: { color: "blue" },
